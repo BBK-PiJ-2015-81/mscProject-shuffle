60abd803ad9373fc7ccd084a0d481d2029f3bd7e
BBK-PiJ-2015-81 <abrait02@dcs.bbk.ac.uk>
2016-08-18 12:54:33 +0100

    Added basic deck selection.

---
+++ deckView.php
@@ -0,0 +1,22 @@
+<?php
+echo "<br>";
+echo "<h2 align = 'center'>Please select a deck to view</h2>";
+echo "<html>";
+    echo "<body>";
+        echo "<div align = 'center''>";
+            echo "<select name='id'>";
+
+                $files = scandir("./decks/");
+                $ignore = array(".", "..");
+
+
+                foreach ($files as $doc) {
+                    if (!in_array($doc, $ignore)) {
+                            echo '<option value="'.$doc.'">'.$doc.'</option>';
+                    }
+                }
+            echo "</select>";
+        echo "</div>";
+    echo "</body>";
+echo "</html>";
+?>
